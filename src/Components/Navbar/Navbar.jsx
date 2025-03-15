bcfac58b959f7639a11ba1d9b23a27c96fae976f
Priya Gupta <priyagupta052k@gmail.com>
2025-03-15 23:42:24 +0530

    Update Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import "./Navbar.css";
 // import Image from "./Logo/Image/Image";
 // import Text from "./Logo/Text/Text";
 import Logo from "../Navbar/Logo/Logo"
+import { IoCart } from "react-icons/io5";
+import {Link} from "react-router-dom";
 
 function Navbar() {
     return (
@@ -12,6 +14,9 @@ function Navbar() {
                 <Text /> */}
                 <Logo />
                 
+            </div>
+            <div className="naright">
+                <div className="cart-container"><Link to="/cart"><IoCart className="cart-icon"/></Link></div>
             </div>
         </div>
     );
