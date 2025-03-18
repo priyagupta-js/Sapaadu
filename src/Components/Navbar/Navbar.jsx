83ad6e8902201f8e17f4eae892a0d3095cf65d8e
Priya Gupta <priyagupta052k@gmail.com>
2025-03-18 23:55:54 +0530

    Update navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -17,9 +17,9 @@ function Navbar() {
                 
             </div>
             <div className="navright">
-            <div className="searchBar-container">
+            {/* <div className="searchBar-container">
                 <input type="text" className="search-bar" placeholder="search ..."/>
-            </div>
+            </div> */}
                 <Link to="/cart"><IoCart className="cart-icon"/></Link>
                 <Link to ="/account"><FaUser className="acct-icon"/></Link>
             </div>
